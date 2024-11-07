ab9b57fe72b0cc1b1d1814d994d4810f5fe65cdd
oltimaloku <oltimaloku555@gmail.com>
2024-11-07 13:16:42 -0800

    Initial Commit

---
+++ ExplainIt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExplainIt
+//
+//  Created by Olti Maloku on 2024-11-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
